5e551971174359d0168d1c4dd5623166c88ba13d
Yang Liu <byliuyang11@gmail.com>
2016-10-17 21:50:17 -0400

    Tune grabber angle and remove RBE2001.ino

--- GrabberController.cpp
+++ GrabberController.cpp
@@ -33,12 +33,12 @@ bool GrabberController::isRotateDownGrabberDone() {
 }
 
 void GrabberController::rotateDownGrabberAtReactor() {
-  transPlaneServo.write(98);
+  transPlaneServo.write(96);
   delay(1000);
 }
 
 bool GrabberController::isRotateDownGrabberAtReactorDone() {
-  return transPlaneServo.read() == 98;
+  return transPlaneServo.read() == 96;
 }
 
 
